15834ec8267170a54dab5b4ba54db7e97d587820
rokcso <rokcsox@gmail.com>
2025-08-19 16:41:32 +0800

    fix: 更新联系邮箱的用户名和域名

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const user = 'rokcso';
-    const domain = 'lunarye.com';
+    const user = 'hi';
+    const domain = 'rokcso.com';
     const emailElement = document.getElementById('email-contact');
     if (emailElement) {
         emailElement.innerHTML = `<a href="mailto:${user}@${domain}">${user}@${domain}</a>`;
